47c3db14d4a5cdd0d0eb023bfa22b95578e88043
Edoardo <55795025+EdoardoFocante@users.noreply.github.com>
2019-10-03 17:19:16 +0200

    Project Creation

---
+++ OOPProject/src/test/java/com/FM/OOPProject/OopProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.FM.OOPProject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OopProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
