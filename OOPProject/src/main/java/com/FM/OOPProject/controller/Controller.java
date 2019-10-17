45644cd34e399f47d1404ea53578158d13398bc4
Edoardo <55795025+EdoardoFocante@users.noreply.github.com>
2019-10-17 17:41:54 +0200

    Final Fixes 1
    Corrected Javadoc and comments
    $bt doesn't require parameters to be in order anymore

--- OOPProject/src/main/java/com/FM/OOPProject/controller/Controller.java
+++ OOPProject/src/main/java/com/FM/OOPProject/controller/Controller.java
@@ -21,12 +21,13 @@ import com.fasterxml.jackson.module.jsonSchema.JsonSchemaGenerator;
 import static com.FM.OOPProject.OopProjectApplication.Cities;
 
 /**
- * Controller che gestisce le richieste REST
+ * Controller che gestisce le richieste
  */
 @RestController
 public class Controller {
 	/**
 	 * Metodo che restituisce l'intero dataset in formato JSON
+	 * 
 	 * @return Dataset non filtrato in formato JSON
 	 */
 	@RequestMapping(value = "/data", method = { RequestMethod.POST, RequestMethod.GET })
@@ -34,10 +35,13 @@ public class Controller {
 		return Cities;
 
 	}
+
 	/**
 	 * Metodo che restituisce i metadati in formato JSON
+	 * 
 	 * @return metadati in formato JSON
-	 * @throws JsonMappingException Lanciata se si verificano errori nel processing dei file JSON
+	 * @throws JsonMappingException Lanciata se si verificano errori nel processing
+	 *                              dei file JSON
 	 */
 	@RequestMapping(value = "/metadata", method = { RequestMethod.POST,
 			RequestMethod.GET }, produces = "application/json")
@@ -47,9 +51,12 @@ public class Controller {
 		JsonSchema schema = schemaGen.generateSchema(City.class);
 		return schema;
 	}
+
 	/**
-	 * Metodo che restituisce un'ArrayList filtrata in formato JSON 
-	 * @param jsonfilter Filtro in formato JSON ottenuto attraverso il body della richiesta POST
+	 * Metodo che restituisce un'ArrayList filtrata in formato JSON
+	 * 
+	 * @param jsonfilter Filtro in formato JSON ottenuto attraverso il body della
+	 *                   richiesta POST
 	 * @return ArrayList contenente i record conformi al filtro inserito
 	 * @throws Exception Eccezioni lanciate dal metodo filteract
 	 */
@@ -57,16 +64,21 @@ public class Controller {
 	public ArrayList<City> getFiltered(@RequestBody(required = true) String jsonfilter) throws Exception {
 		return filteract(Cities, jsonfilter); // Parsing del request body
 	}
+
 	/**
-	 * Metodo che restituisce le statistiche di una 
-	 * @param year Anno per la quale si vogliono ottenere le statistiche. Se omesso viene restituito il calcolo anno per anno
-	 * @param jsonfilter Filtro in formato JSON ottenuto attraverso il body della richiesta POST
+	 * Metodo che restituisce le statistiche del dataset dopo aver applicato un
+	 * filtro
+	 * 
+	 * @param year       Anno per la quale si vogliono ottenere le statistiche. Se
+	 *                   omesso viene restituito il calcolo anno per anno
+	 * @param jsonfilter Filtro in formato JSON ottenuto attraverso il body della
+	 *                   richiesta POST
 	 * @return Statistiche in formato JSON ottenute da una lista filtrata
 	 * @throws Exception Eccezioni lanciate dal metodo filteract
 	 */
 	@RequestMapping(value = "/stats", produces = "application/json")
 	public ArrayList<Statistics> getStats(@RequestParam(required = false, defaultValue = "-1") int year,
-			@RequestBody(required=true) String jsonfilter) throws Exception {
+			@RequestBody(required = true) String jsonfilter) throws Exception {
 		ArrayList<Statistics> result = new ArrayList<Statistics>();
 		ArrayList<City> filtered = filteract(Cities, jsonfilter);
 		if (year == -1) { // anno non inserito, statistiche fatte per ogni anno
@@ -79,63 +91,73 @@ public class Controller {
 		}
 		return result;
 	}
+
 	/**
-	 * Metodo che effettua il parsing delle condizioni and e or del filtro e le attua.
-	 * Se non sono presenti richiama solamente la funzione successiva
-	 * @param data ArrayList dei dati su cui operare il filtro
-	 * @param jsonfilter filtro in formato JSON ottenuto attraverso il body della richiesta POST
+	 * Metodo che effettua il parsing delle condizioni and e or del filtro e le
+	 * attua. Se non sono presenti richiama solamente la funzione successiva
+	 * 
+	 * @param data       ArrayList dei dati su cui operare il filtro
+	 * @param jsonfilter filtro in formato JSON ottenuto attraverso il body della
+	 *                   richiesta POST
 	 * @return Lista filtrata
 	 * @throws Exception Eccezzioni lanciate dal metodo filteract1
 	 */
 	private ArrayList<City> filteract(ArrayList<City> data, String jsonfilter) throws Exception {
 		ArrayList<City> filtered = new ArrayList<City>();
 		JSONObject f = new JSONObject(jsonfilter);
-		if(f.length() != 0) { //controllo che il filtro non sia vuoto
-		JSONArray arr; // Parsing del request body
-		if (f.has("$or") && f.getJSONArray("$or").length()>0) { //Seconda condizione per evitare malfunzionamenti in caso di sezioni or vuote
-			arr = f.getJSONArray("$or");
-			for (int i = 0; i < arr.length(); i++) {
-				System.out.println(arr.getJSONObject(i).toString());
-				filtered = filteract1(arr.getJSONObject(i), data, filtered);
-				// per l'or filtro parto dal risultato e aggiungo tutti gli
-				// elementi non già contenuti che soddisfano la nuova condizione
+		if (f.length() != 0) { // controllo che il filtro non sia vuoto
+			JSONArray arr;
+			if (f.has("$or") && f.getJSONArray("$or").length() > 0) { // Seconda condizione per evitare malfunzionamenti
+																		// in caso di sezioni or vuote
+				arr = f.getJSONArray("$or");
+				for (int i = 0; i < arr.length(); i++) {
+					System.out.println(arr.getJSONObject(i).toString());
+					filtered = filteract1(arr.getJSONObject(i), data, filtered);
+					// per l'or filtro parto dal risultato e aggiungo tutti gli
+					// elementi non già contenuti che soddisfano la nuova condizione
 
+				}
+			} else
+				filtered = data; // se non ho una parte or, il vettore di partenza per le and è l'intera
+								 // arraylist dei dati
+			if (f.has("$and")) {
+				arr = f.getJSONArray("$and");
+				for (int i = 0; i < arr.length(); i++) {
+					ArrayList<City> emptystart = new ArrayList<City>();
+					filtered = filteract1(arr.getJSONObject(i), filtered, emptystart);
+					// per l'and filtro la stessa stringa più volte perchè rispetti tutte
+					// le condizioni, aggiungo i nuovi record in una stringa vuota ad ogni
+					// iterazione
+				}
 			}
-		} else
-			filtered = data; // se non ho una parte or, il vettore di partenza per le and è l'intera
-								// arraylist dei dati
-		if (f.has("$and")) {
-			arr = f.getJSONArray("$and");
-			for (int i = 0; i < arr.length(); i++) {
-				ArrayList<City> emptystart = new ArrayList<City>();
-				System.out.println(arr.getJSONObject(i).toString());
-				filtered = filteract1(arr.getJSONObject(i), filtered, emptystart); 
-				// per l'and filtro la stessa stringa più volte perchè rispetti tutte
-				// le condizioni, aggiungo i nuovi record in una stringa vuota ad ogni iterazione
-			}
-		}
-		if (f.has("$or") || f.has("$and")) {
+			if (f.has("$or") || f.has("$and")) {
+				return filtered;
+			} else // se non ho nè and nè or elaboro soltanto la singola richiesta
+				filtered = filteract1(f, data, new ArrayList<City>());
 			return filtered;
 		} else
-			filtered = filteract1(f, data, new ArrayList<City>());
-		return filtered;
-		}else return data; //se il filtro è vuoto non attuo alcun filtraggio
-	
+			return data; // se il filtro è vuoto non attuo alcun filtraggio
+
 	}
+
 	/**
 	 * Metodo che effettua il parsing delle singole condizioni del filtro e le attua
+	 * 
 	 * @param Condition JSONObject contenente la singola condizione
-	 * @param src ArrayList dei dati da filtrare
-	 * @param in ArrayList di partenza a cui aggiungere i nuovi record conformi al filtro se non già presenti
+	 * @param src       ArrayList dei dati da filtrare
+	 * @param in        ArrayList di partenza a cui aggiungere i nuovi record
+	 *                  conformi al filtro se non già presenti
 	 * @return ArrayList di partenza con aggiunti i nuovi record conformi al filtro
-	 * @throws Exception Eccezioni lanciate dal metodo select, e ResponseStatusException se vengono inserite più istruzioni senza
-	 *	racchiuderle in un and oppure un or
+	 * @throws Exception Eccezioni lanciate dal metodo select, e
+	 *                   ResponseStatusException se vengono inserite più istruzioni
+	 *                   senza in un and oppure un or
 	 */
 	private ArrayList<City> filteract1(JSONObject Condition, ArrayList<City> src, ArrayList<City> in) throws Exception {
 		if (Condition.length() < 2) { // controllo se ho un array di condizioni non racchiuso in un and o un or
 			String fieldname = Condition.names().get(0).toString();
 			String op = Condition.getJSONObject(fieldname).names().get(0).toString();
-			if (Condition.getJSONObject(fieldname).get(op) instanceof JSONArray) { // il value dell'operatore può essere un singolo oggetto od un array
+			if (Condition.getJSONObject(fieldname).get(op) instanceof JSONArray) { // il value dell'operatore può essere
+																					// un singolo oggetto od un array
 				JSONArray value = Condition.getJSONObject(fieldname).getJSONArray(op);
 				Object[] objvalue = new Object[value.length()];
 				for (int i = 0; i < value.length(); i++) { // converto il valore il jsonarray in un array di oggetti
@@ -145,12 +167,11 @@ public class Controller {
 
 			} else {
 				Object objvalue = Condition.getJSONObject(fieldname).get(op);
-
-				return FilterUtils.select(src, in, fieldname, op , objvalue);
+				return FilterUtils.select(src, in, fieldname, op, objvalue);
 			}
 		} else
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
-					"To apply a filter with more than one condition add an \"and\" or an \"or\"section to your filter");
+					"To apply a filter with more than one condition add an $and or an $or section to your filter");
 
 	}
 }
--- OOPProject/src/main/java/com/FM/OOPProject/utilities/FilterUtils.java
+++ OOPProject/src/main/java/com/FM/OOPProject/utilities/FilterUtils.java
@@ -1,7 +1,5 @@
 package com.FM.OOPProject.utilities;
 
-
-
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
@@ -17,17 +15,19 @@ import com.FM.OOPProject.model.City;
 public class FilterUtils {
 	/**
 	 * Funzione che verifica una determinata condizione del filtro
-	 * @param value valore del dato da confrontare con il parametro del filtro
+	 * 
+	 * @param value    valore del record da confrontare con il parametro del filtro
 	 * @param operator operatore da utilizzare per il confronto
-	 * @param par parametro o array di parametri del filtro
+	 * @param par      parametro o array di parametri del filtro
 	 * @return true se la condizione è verificata, false se non lo è
-	 * @throws ResponseStatusException se i parametri della richiesta non sono adatti al rispettivo operatore
+	 * @throws ResponseStatusException se i parametri della richiesta non sono
+	 *                                 adatti al rispettivo operatore
 	 */
 	private static boolean check(Object value, String operator, Object... par) {
-		if (par.length==1) {
+		if (par.length == 1) { // se il parametro della condizione è costituito da un solo elemento
 			if (value instanceof Number && par[0] instanceof Number) {
-				Float valueF = ((Number)value).floatValue();
-				Float parF = ((Number)par[0]).floatValue();
+				Float valueF = ((Number) value).floatValue();
+				Float parF = ((Number) par[0]).floatValue();
 				switch (operator) {
 				case "$eq":
 				case "$in":
@@ -40,15 +40,16 @@ public class FilterUtils {
 				case "$gte":
 					return valueF >= parF;
 				case "$lt":
-					return (valueF < parF && valueF>0);
+					return (valueF < parF && valueF > 0);
 				case "$lte":
-					return (valueF <= parF && valueF>0);
+					return (valueF <= parF && valueF > 0);
 				case "$bt":
-					throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"between operator $bt requires two parameters");
+					throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+							"between operator $bt requires two parameters");
 				default:
-					throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Invalid operator");
-				}
-			} else if (value instanceof String && par[0] instanceof String) {
+					throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid operator");
+				} 
+			} else if (value instanceof String && par[0] instanceof String) { 
 				switch (operator) {
 				case "$eq":
 				case "$in":
@@ -57,63 +58,76 @@ public class FilterUtils {
 				case "$nin":
 					return !(value.equals(par[0]));
 				default:
-					throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Operator "+ operator+" is not compatible with string type");
-				} 
-			} else throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Invalid parameter format");
-		} else { // par is made of more than one element
+					throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+							"Operator " + operator + " is not valid or is not compatible with string type");
+				}
+			} else
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid parameter format");
+		} else { // se la condizione ha un array di parametri
 			switch (operator) {
 			case "$in":
 				return Arrays.asList(par).contains(value);
 			case "$nin":
 				return !(Arrays.asList(par).contains(value));
 			case "$bt":
-				if(par.length==2 && par[0] instanceof Number && par[1] instanceof Number) {
-					Float valueF = ((Number)value).floatValue();
-					Float parF = ((Number)par[0]).floatValue();
-					Float ub = ((Number)par[1]).floatValue(); //Upper Bound
-					return (valueF <= ub && valueF >=parF);
-				}
-				else throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"$bt operator requires 2 numeric parameters");
-			default: 
-				throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Operator "+ operator+" not compatible with parameters");
-
+				if (par.length == 2 && par[0] instanceof Number && par[1] instanceof Number) {
+					Float valueF = ((Number) value).floatValue();
+					Float parF = ((Number) par[0]).floatValue();
+					Float par2F = ((Number) par[1]).floatValue(); // Secondo Parametro
+					return (valueF <= Math.max(parF, par2F) && valueF >= Math.min(parF, par2F));
+				} else
+					throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+							"$bt operator requires 2 numeric parameters");
+			default:
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+						"Operator " + operator + " not valid or incompatible with parameters");
 
 			}
 		}
 	}
+
 	/**
+	 * Metodo che aggiunge alla lista in tutti i record della sorcente src che
+	 * soddisfano i parametri in ingresso
 	 * 
-	 * @param src Collection di oggetti di tipo City su cui operare il filtraggio
-	 * @param in ArrayList di partenza a cui aggiungere i nuovi elementi che soddisfano i parametri
+	 * @param src       Collection di oggetti di tipo City su cui operare il
+	 *                  filtraggio
+	 * @param in        ArrayList di partenza a cui aggiungere i nuovi elementi che
+	 *                  soddisfano i parametri
 	 * @param fieldName Nome dell'attributo su cui effettuare il confronto
-	 * @param operator Operatore con cui effetuare il confronto
-	 * @param value Parametro o array di parametri del fitro
-	 * @throws IllegalAccessException Lanciata la funzione invoke non ha accesso alla definizione del metodo m
-	 * @throws IllegalArgumentException Lanciata se una funzione riceve un parametro non appropriato
-	 * @throws InvocationTargetException Lanciata se il metodo che invoco tramite invoke lancia a sua volta un Exception
-	 * @throws SecurityException Lanciata se getMethod prova ad accedere ad una risorsa per cui non ha sufficienti autorizzazioni 
-	 * @throws NoSuchMethodException Lanciata se viene fatto un riferimento ad un metodo non esistente
+	 * @param operator  Operatore con cui effetuare il confronto
+	 * @param value     Parametro o array di parametri del fitro
+	 * @throws IllegalAccessException    Lanciata la funzione invoke non ha accesso
+	 *                                   alla definizione del metodo m
+	 * @throws IllegalArgumentException  Lanciata se una funzione riceve un
+	 *                                   parametro non appropriato
+	 * @throws InvocationTargetException Lanciata se il metodo che invoco tramite
+	 *                                   invoke lancia a sua volta un Exception
+	 * @throws SecurityException         Lanciata se getMethod prova ad accedere ad
+	 *                                   una risorsa per cui non ha sufficienti
+	 *                                   autorizzazioni
+	 * @throws NoSuchMethodException     Lanciata se viene fatto un riferimento ad
+	 *                                   un metodo non esistente
 	 */
-	public static ArrayList<City> select(ArrayList<City> src, ArrayList<City> in, String fieldName, String operator, Object... value) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException  {
+	public static ArrayList<City> select(ArrayList<City> src, ArrayList<City> in, String fieldName, String operator,
+			Object... value) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException,
+			NoSuchMethodException, SecurityException {
 		ArrayList<City> out = in;
-		for(City item:src) {
-				Object tmp = new Object();
-				if (Pattern.matches("^\\d+$",fieldName)){
-					int year = Integer.parseInt((String) fieldName);
-					tmp = item.getYearData(year);
-				} 
-				else {
-					Method m = item.getClass().getMethod("get"+fieldName.substring(0,1).toUpperCase()+fieldName.substring(1), null);
-					tmp = m.invoke(item);
-				}
-				if(FilterUtils.check(tmp, operator, value) && !(out.contains(item))) {
-					out.add(item);
-				}
-				
-				
+		for (City item : src) {
+			Object tmp = new Object();
+			if (Pattern.matches("^\\d+$", fieldName)) { //se field è un anno uso il metodo getYearData
+				int year = Integer.parseInt((String) fieldName);
+				tmp = item.getYearData(year);
+			} else { // se field è "citycode" o "indic_ur" uso il metodo get apposito
+				Method m = item.getClass()
+						.getMethod("get" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1), null);
+				tmp = m.invoke(item);
+			}
+			if (FilterUtils.check(tmp, operator, value) && !(out.contains(item))) {
+				out.add(item);
+			}
 
 		}
 		return out;
-		}
-	} 
-
+	}
+}
